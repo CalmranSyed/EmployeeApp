4c4916540f2d40228e83d54effd4640ef06cbeb3
Syed Kamran Ahmed <ksyedexd@gmail.com>
2022-02-02 01:10:37 +0530

    added more markup and some js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-function dasboard() {
-    
-}
+$(document).ready(function () {
+    $("#add-emp").click(function () {
+        $("#add-form").toggleClass("d-block d-none");
+    });
+    $("#update-emp").click(function () {
+        $("#update-form").toggleClass("d-block d-none");
+    });
+    $("#list-emp").click(function () {
+        $("#emp-list").toggleClass("d-block d-none");
+    });
+    $("#search-emp").click(function () {
+        $("#search-form").toggleClass("d-block d-none");
+    });
+    $("#delete-emp").click(function () {
+        $("#delete-form").toggleClass("d-block d-none");
+    });
+});
+
