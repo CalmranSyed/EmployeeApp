c2d9d1f42d35c6f1fcf28ee40e0f9e59c8942878
Syed Kamran Ahmed <ksyedexd@gmail.com>
2022-02-06 00:54:18 +0530

    Fixed form validation and added local storage in required format for key value pairs

--- js/main.js
+++ js/main.js
@@ -1,35 +1,37 @@
 $(document).ready(function () {
     function empapp() {
 
-        var add_form = $("#add-form").value;
-        var employee_id = $("#emp-id").value;
-        var employee_name = $("#emp-name").value;
-        var employee_phone = $("#emp-phone").value;
-        var employee_email = $("#emp-email").value;
-
-        var nameCheck = /[a-zA-Z]/;
-        var mailCheck = /^([a-z0-9!#$%&'*+\-/=?^_`{|}~]+(?:\.[a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~]+)*)@((?:[a-z0-9]+(?:[a-z-0-9-]*)\.)+[a-z]{2,})$/gi;
-
-        $("#add-form .submitbtn").click(function (ev) {
+        
+        $("#add-form #submitbtn").click(function (ev) {
             ev.preventDefault();
+            
+            var add_form = $("#add-form");
+            var employee_id = $("#emp-id").val();
+            var employee_name = $("#emp-name").val();
+            var employee_phone = $("#emp-phone").val();
+            var employee_email = $("#emp-email").val();
+    
+            var nameCheck = /[a-zA-Z]/;
+            var mailCheck = /^([a-z0-9!#$%&'*+\-/=?^_`{|}~]+(?:\.[a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~]+)*)@((?:[a-z0-9]+(?:[a-z-0-9-]*)\.)+[a-z]{2,})$/gi;
 
-            if ($("#add-form input").value == "") {
-                $("#add-form .error").text = "Please fill the form fields and accept the terms and conditions";
+            if ($("#add-form input").val() == "") {
+                $(".error").text("Please fill all form fields before submitting");
+                console.log($(".error").text());
             }
         
             else if (!nameCheck.test(employee_name)) {
-                $("#add-form .error").text = "Please enter a valid name";
+                $("#add-form .error").text("Please enter a valid name");
             }
         
             else if (!mailCheck.test(employee_email)) {
-                $("#add-form .error").text = "Please enter a valid email address";
+                $("#add-form .error").text("Please enter a valid email address");
             }
             
             else {
-                $("#add-form .error").text = "";
-                $("#add-form .success").text = "Employee data for "+employee_name+" created with ID "+employee_id;
+                $("#add-form .error").text("");
+                $("#add-form .success").text("Employee data for "+employee_name+" created with ID "+employee_id);
         
-                localStorage.setItem(employee_id,{employee_name , employee_phone , employee_email});
+                localStorage.setItem(employee_id,JSON.stringify({"name":employee_name , "phone-no.":employee_phone , "email-id":employee_email}));
                 
             }
         });
